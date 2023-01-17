769055902c2d79f25110973a44a614c58a6e980f
miemie <2380848997@qq.com>
2023-01-18 01:44:02 +0800

    feat:工具函数

---
+++ src/utils/index.ts
@@ -0,0 +1,35 @@
+import { Model, HydratedDocument } from 'mongoose'
+import { CommentQueryInfo, CardQueryInfo } from '@/types'
+
+export const queryComments = async <K, T extends Model<HydratedDocument<K>>>(model: T, commentQueryInfo: CommentQueryInfo): Promise<Array<K>> => {
+    const limit: number = parseInt(commentQueryInfo.limit)
+    const currentPage: number = parseInt(commentQueryInfo.currentPage)
+    const card: string = commentQueryInfo.card
+    const select: string = 'avatar name content time'
+    const result: Array<K> = await model.find({ card }).
+        select(select).sort('-time').skip(currentPage * limit).limit(limit)
+    return result
+}
+export const queryCards = async <K, T extends Model<HydratedDocument<K>>>(model: T, cardQueryInfo: CardQueryInfo, select: string): Promise<Array<K>> => {
+    const limit: number = parseInt(cardQueryInfo.limit)
+    const currentPage: number = parseInt(cardQueryInfo.currentPage)
+    const label: number = parseInt(cardQueryInfo.label)
+    const query: { label?: number } = label === -1 ? {} : { label }
+    const result: Array<K> = await model.find(query).
+        select(select).sort('-time').skip(currentPage * limit).limit(limit).
+        populate('commentNumber')
+    return result
+}
+export const likeCard = async<K extends { liked: Array<string> }, T extends Model<HydratedDocument<K>>>(_id: string, visitorId: string, model: T) => {
+    const document: HydratedDocument<K> = await model.findById(_id)
+    const index = document.liked.indexOf(visitorId)
+    if (index === -1) {
+        document.liked.push(visitorId)
+        await document.save()
+        return { state: 200, data: '点赞成功' } as const
+    } else {
+        document.liked.splice(index, 1)
+        await document.save()
+        return { state: 200, data: '已取消赞' } as const
+    }
+}
